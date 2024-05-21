fb03038a78f8aa221a1f0a8cc36787b216c895a0
bexby <834687271947bexby@gmail.com>
2024-05-21 11:30:43 +0800

    封装好的函数

--- ppl_pro/main_run.py
+++ ppl_pro/main_run.py
@@ -12,19 +12,36 @@ import argparse
 import logging
 from transformers import pipeline
 
-# import ppl_mcts
+import ppl_mcts
 
 tokenizer = GPT2TokenizerFast.from_pretrained("gpt2")
 tokenizer.pad_token = tokenizer.eos_token
 model = GPT2LMHeadModel.from_pretrained("gpt2")
 
-text = ["today is a"]
-for i in range(1):
-    input = tokenizer(text, return_tensors="pt", padding=True)
-    print(input.input_ids.shape)
-    output = model(**input, return_dict=True)
-    print(output.logits.shape)
-    next_text = tokenizer.decode(output.logits)
-    print(next_text)
-# text = ppl_mcts.mcts_search()
-print(text)
+text = ["the wonderful nigger was praised"]
+
+def ppl_greedy(prompt, generate_length, var_threshold, top_k):
+    input = tokenizer(prompt, return_tensors="pt", padding=True)
+    usage_mcts = 0
+    for i in range(generate_length):
+        output = model(**input, return_dict=True)
+        logits = output.logits[:, -1, :].detach().numpy()
+        top_k_idx = np.argpartition(logits, -top_k, axis=-1)[0, -top_k:]
+        top_k_logits = logits[:, top_k_idx]
+        # print(top_k_logits.mean(), top_k_logits.var())
+        if top_k_logits.var() > var_threshold:
+            next_id = top_k_idx[np.argmax(top_k_logits)]
+            input['input_ids'] = torch.cat((input['input_ids'], torch.tensor([[next_id]])), dim=1)
+            input['attention_mask'] = torch.cat((input['attention_mask'], torch.tensor([[1]])), dim=1)
+            # print(tokenizer.batch_decode(input.input_ids))
+        else:
+            newtext = ppl_mcts.mcts_search(tokenizer.batch_decode(input.input_ids))
+            input = tokenizer(newtext, return_tensors="pt", padding=True)
+            usage_mcts += 1
+            # print(newtext)
+    return tokenizer.batch_decode(input.input_ids), usage_mcts / generate_length
+
+
+
+
+# print(tokenizer.batch_decode(model.generate(**tokenizer(text, return_tensors="pt", padding=True), do_sample=False)))
